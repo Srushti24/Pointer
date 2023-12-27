26b381956b0e544d72fc4967b2d2f59cdec9767a
Srushti24 <srushti.desai@gmail.com>
2023-12-27 17:46:23 -0500

    Initial Commit

---
+++ Pointer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pointer
+//
+//  Created by Srushti Desai on 12/27/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
