e330a5b8f6dc29acb9a9073b6d706696aee54ce1
Srushti24 <srushti.desai@gmail.com>
2024-09-19 21:18:50 -0400

    finished unique pointer

--- src/main.cpp
+++ src/main.cpp
@@ -15,16 +15,15 @@ int main(int argc, const char * argv[]) {
     UniquePointer<int> ptr1;
     UniquePointer<int>* ptr2 = new UniquePointer<int>();
     UniquePointer<int> ptr3 = make_unique<int>();
-//    std::vector<UniquePointer<int> > unique_ptrs;
-//     for(size_t i=0; i< 7; i++)
-//     {
-//         unique_ptrs.push_back(make_unique<int>(i));
-//     }
+    std::vector<UniquePointer<int> > testArray;
+    for (int i = 0; i < 3; ++i) {
+        testArray.push_back(UniquePointer<int>(new int(i)));
+    }
+
+     for (auto& ptr : testArray) {
+        std::cout << *ptr << "\n";
+    }
     *ptr3 = 6;
-    // for(size_t i=0; i< 7; i++)
-    // {
-    //     std::cout <<  *unique_ptrs[i] << "\n";
-    //}
     std::cout << "value is \n" ;
     std::cout << *ptr3  << "\n";
 
