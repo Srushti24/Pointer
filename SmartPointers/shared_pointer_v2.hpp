02737ca1e7ec848468e759f21ce29d711f2f7ff9
Srushti24 <srushti.desai@gmail.com>
2024-10-20 17:28:38 -0400

    clang-format ran

--- SmartPointers/shared_pointer_v2.hpp
+++ SmartPointers/shared_pointer_v2.hpp
@@ -8,28 +8,23 @@ template <typename T> class SharedPointerV2 {
     SharedPointerV2() : m_ptr(nullptr), m_count(nullptr) { std::cout << "inside constructor \n"; };
 
     // Destructor
-    ~SharedPointerV2() { 
-      destroy(); 
-      }
+    ~SharedPointerV2() { destroy(); }
 
     void destroy() {
-        std::cout <<"inside destroy \n";
+        std::cout << "inside destroy \n";
         if (m_count != nullptr) {
-            *m_count =  *m_count -1;
-            if(*m_count == 0 )
-            {
-              delete m_ptr;
-              m_ptr = nullptr;
-              delete m_count;
-              m_count = nullptr;
+            *m_count = *m_count - 1;
+            if (*m_count == 0) {
+                delete m_ptr;
+                delete m_count;
             }
-          }
+        }
     }
 
     // Param constructor
-    SharedPointerV2(T* temp) : m_ptr(temp){ 
-      m_count = temp == nullptr ? nullptr : new int(1);
-      std::cout << "inside param constructor \n"; 
+    SharedPointerV2(T* temp) : m_ptr(temp) {
+        m_count = temp == nullptr ? nullptr : new int(1);
+        std::cout << "inside param constructor \n";
     }
 
     // Copy Assigment operator
@@ -38,18 +33,16 @@ template <typename T> class SharedPointerV2 {
         destroy();
         m_count = temp.m_count;
         m_ptr   = temp.m_ptr;
-        if(m_count != nullptr)
-          {
+        if (m_count != nullptr) {
             *m_count = *m_count + 1;
-          }
+        }
         return *this;
     }
 
     // Copy constructor
     SharedPointerV2(const SharedPointerV2& temp) : m_ptr(temp.m_ptr), m_count(temp.m_count) {
-        if(m_ptr != nullptr)
-        {
-          *m_count = *m_count+1;
+        if (m_ptr != nullptr) {
+            *m_count = *m_count + 1;
         }
     }
 
@@ -62,32 +55,24 @@ template <typename T> class SharedPointerV2 {
 
     // Move Assignmenet operator
     SharedPointerV2 operator=(SharedPointerV2&& temp) {
-        std::cout << "Move Assignmenet operator \n" ;
+        std::cout << "Move Assignmenet operator \n";
         destroy();
         m_count      = temp.m_count;
         m_ptr        = temp.m_ptr;
         temp.m_count = nullptr;
         temp.m_ptr   = nullptr;
     }
-   
-    T* operator->()
-    {
-      return m_ptr;
-    }
 
-     T* get()
-    {
-      return m_ptr;
-    }
+    T* operator->() { return m_ptr; }
 
-    T& operator*()
-    {
-      return *m_ptr;
-    }
+    T* get() { return m_ptr; }
+
+    T& operator*() { return *m_ptr; }
+
   private:
     T* m_ptr;
-    //Invarient: When m_ptr is nullptr, m_count will be nullptr
-    //When m_ptr is not null @m_count will not be null and the reference value of the @m_count pointer
-    //will be minimum 1.
+    // Invarient: When m_ptr is nullptr, m_count will be nullptr
+    // When m_ptr is not null @m_count will not be null and the reference value of the @m_count pointer
+    // will be minimum 1.
     int* m_count;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,41 +6,33 @@
 //
 
 #include "Box.hpp"
-#include "shared_pointer.hpp"
-#include "unique_pointer.hpp"
 #include "my_pointer.hpp"
+#include "shared_pointer.hpp"
 #include "shared_pointer_v2.hpp"
+#include "unique_pointer.hpp"
+#include <cassert>
 #include <iostream>
 #include <vector>
-#include <cassert>
 
-
-
-void testUniquePointer()
-{
-     UniquePointer<int> uniquePointer = make_unique<int>(5);
-     UniquePointer<int> uniquePointerOne = make_unique<int>(10);
-     assert(*uniquePointer == 5);
-     assert(*uniquePointerOne == 10);
-     uniquePointer = (std::move(uniquePointerOne));
-     assert(*uniquePointer == 10);
-
+void testUniquePointer() {
+    UniquePointer<int> uniquePointer    = make_unique<int>(5);
+    UniquePointer<int> uniquePointerOne = make_unique<int>(10);
+    assert(*uniquePointer == 5);
+    assert(*uniquePointerOne == 10);
+    uniquePointer = (std::move(uniquePointerOne));
+    assert(*uniquePointer == 10);
 }
 
-void testUniqueArrayPtr()
-{
+void testUniqueArrayPtr() {
     std::vector<UniquePointer<int>> intVectors;
-    for(size_t i =0; i< 5 ; i++)
-    {
+    for (size_t i = 0; i < 5; i++) {
         intVectors.push_back(make_unique<int>(i));
     }
     std::cout << "intVector size is " << intVectors.size() << "\n";
 }
 
-
-void testSharedPointer()
-{
-    SharedPointer<int> shared_pointer = make_shared<int>(5);
+void testSharedPointer() {
+    SharedPointer<int> shared_pointer     = make_shared<int>(5);
     SharedPointer<int> shared_pointer_one = make_shared<int>(10);
     assert(*shared_pointer == 5);
     assert(*shared_pointer_one == 10);
@@ -48,122 +40,89 @@ void testSharedPointer()
     assert(*shared_pointer == 10);
 }
 
-void testUniquePointerStruct()
-{
+void testUniquePointerStruct() {
     int constructionCount = 0;
-    int destructionCount = 0;
+    int destructionCount  = 0;
 
-    struct S{
-        S(int& constructionCount, int& destructionCount): constructionCount(constructionCount), destructionCount(destructionCount)
-        {
-            std::cout << "constructionCount \n" ;
+    struct S {
+        S(int& constructionCount, int& destructionCount)
+            : constructionCount(constructionCount), destructionCount(destructionCount) {
+            std::cout << "constructionCount \n";
             constructionCount++;
         }
-        ~S(){
-            std::cout << "destructionCount \n" ;
+        ~S() {
+            std::cout << "destructionCount \n";
             destructionCount++;
         }
         int constructionCount;
-        int destructionCount; 
+        int destructionCount;
     };
 
     std::vector<UniquePointer<S>> vectorS;
-    for(size_t i =0; i< 5; i++)
-    {
+    for (size_t i = 0; i < 5; i++) {
         vectorS.push_back(make_unique<S>(constructionCount, destructionCount));
     }
     assert(constructionCount == 5);
-    assert( destructionCount == 0);
+    assert(destructionCount == 0);
 }
 
-template <typename T, typename... Args> 
-UniquePointer<T> createUniquePtr(Args&&... args)
-{
+template <typename T, typename... Args> UniquePointer<T> createUniquePtr(Args&&... args) {
     return make_unique<T>(std::forward<Args>(args)...);
 }
 
-UniquePointer<int> createIntPtr()
-{
+UniquePointer<int> createIntPtr() {
     UniquePointer<int> temp;
     return temp;
 }
 
-template<typename T>
-void forTesting(UniquePointer<T> args)
-{
-    std::cout << "value is " << *args << "\n";
-}
+template <typename T> void forTesting(UniquePointer<T> args) { std::cout << "value is " << *args << "\n"; }
 
-
-void testForTesting()
-{
+void testForTesting() {
     SharedPointer<int> temp1;
-    int* temp =  new int(5);
+    int*               temp = new int(5);
     SharedPointer<int> temp2(temp);
 }
 
-void testSharedPointerTwo()
-{
+void testSharedPointerTwo() {
     SharedPointer<int> temp;
     {
-    auto temp2 = SharedPointer<int>(new int{5});
-    temp = temp2;
+        auto temp2 = SharedPointer<int>(new int{5});
+        temp       = temp2;
     }
-    std::cout<< "temp value is " << *temp << std::endl;
-  
+    std::cout << "temp value is " << *temp << std::endl;
 }
 
-void testSharedV2One()
-{
+void testSharedV2One() {
     SharedPointerV2<int> temp;
     {
         auto temp1 = SharedPointerV2<int>(new int{5});
-        temp = temp1;
+        temp       = temp1;
         std::cout << "assignment done" << std::endl;
     }
     std::cout << "temp1 val is  " << *temp << std::endl;
 }
 
-void testSharedV2Two()
-{
-  SharedPointerV2<int> s1;
-  SharedPointerV2<int> s2;
-  s1 = s2;  
+void testSharedV2Two() {
+    SharedPointerV2<int> s1;
+    SharedPointerV2<int> s2;
+    s1 = s2;
 }
 
-void testSharedV2Three()
-{
-    
-}
+void testSharedV2Three() {}
 
-void testSharedV2Four()
-{
-    
-}
+void testSharedV2Four() {}
 
-void testSharedV2Five()
-{
-    
-}
+void testSharedV2Five() {}
 
-void testSharedV2Six()
-{
-    
-}
-
+void testSharedV2Six() {}
 
 int main(int argc, const char* argv[]) {
-//    testUniquePointer();
-//     testSharedPointer();
-//    testUniquePointerStruct();
-//    testUniqueArrayPtr();
- //testSharedPointerTwo();
-      testSharedV2One();
-      testSharedV2Two();
-  return 0;
+    //    testUniquePointer();
+    //     testSharedPointer();
+    //    testUniquePointerStruct();
+    //    testUniqueArrayPtr();
+    // testSharedPointerTwo();
+    testSharedV2One();
+    testSharedV2Two();
+    return 0;
 }
-
-
-
-
-
